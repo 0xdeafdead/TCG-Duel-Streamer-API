bfe50ac1a3d2e0e2e97ff114f02001f381dcf470
Adolfo Alcantara <0xdeafdead@Adolfos-MacBook-Pro.local>
2024-12-01 01:52:23 -0600

    feat: started implementing websocket

--- src/match/match.controller.ts
+++ src/match/match.controller.ts
@@ -1,15 +1,21 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { MatchService } from './match.service';
 import { Channel } from 'src/types/Channel';
 import { Observable } from 'rxjs';
 import { StartMatchInput } from './DTOs/StartMatchInput.dto';
+import { GatewayData } from 'src/types';
 
 @Controller('match')
 export class MatchController {
     constructor(private readonly service: MatchService) { }
 
     @Post()
-    createNewVoiceChannel(@Body("input") input: StartMatchInput): Observable<Channel> {
+    createNewVoiceChannel(@Body("input") input: StartMatchInput): Observable<string> {
         return this.service.startMatch(input);
+    }
+
+    @Get()
+    getBotGateway(): Observable<GatewayData> {
+        return this.service.startVoiceConnections();
     }
 }
--- src/match/match.service.ts
+++ src/match/match.service.ts
@@ -1,7 +1,7 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable, InternalServerErrorException } from '@nestjs/common';
-import { catchError, map, Observable, of, throwError } from 'rxjs';
-import { Channel } from '../types';
+import { catchError, EMPTY, forkJoin, map, Observable, of, switchMap, throwError } from 'rxjs';
+import { Channel, GatewayData } from '../types';
 import { StartMatchInput } from './DTOs/StartMatchInput.dto';
 
 
@@ -9,7 +9,60 @@ import { StartMatchInput } from './DTOs/StartMatchInput.dto';
 export class MatchService {
     constructor(private readonly httpService: HttpService) { }
 
-    startMatch(input: StartMatchInput): Observable<Channel> {
+    startMatch(input: StartMatchInput) {
+        return forkJoin([this.createVoiceChannel(input), this.startVoiceConnections()]).pipe(
+            switchMap(([newChannel, gatewayData]) => {
+                const voiceStatusSocket = new WebSocket(gatewayData.url);
+                voiceStatusSocket.onopen = event => {
+                    voiceStatusSocket.send(JSON.stringify({ op: 4, d: { guild_id: process.env.SERVER_ID, channel_id: newChannel.id, self_mute: false, self_deaf: false } }))
+                }
+                let isReady = 2
+                let voiceConnectionSocket: WebSocket;
+                let sessionId: string;
+                let endpoint: "wss://";
+                let token: string;
+                voiceStatusSocket.onmessage = (event) => {
+                    const data = JSON.parse(event.data);
+                    console.log(event);
+                    console.log("event data", data);
+                    if (data.t === "VOICE_SERVER_UPDATE") {
+                        token = data.token;
+                        endpoint += data.endpoint;
+                        isReady--;
+                    }
+                    if (data.t === "VOICE_STATE_UPDATE") {
+                        sessionId = data.session_id;
+                        isReady--;
+                    }
+                    if (isReady === 0) {
+                        voiceConnectionSocket = new WebSocket(endpoint);
+                        voiceConnectionSocket.onopen = event => {
+                            voiceConnectionSocket.send(JSON.stringify({
+                                op: 0, d: {
+                                    server_id: process.env.SERVER_ID,
+                                    user_id: "1311844705063141486",
+                                    session_id: sessionId,
+                                    token
+                                }
+                            }))
+                        }
+
+                        voiceConnectionSocket.onmessage = event => {
+                            const data = event.data;
+                            console.log('data', data)
+                        }
+                    }
+                }
+
+                return of("testing");
+            }), catchError((err) => {
+                console.log('err', err)
+                return throwError(() => new InternalServerErrorException(err.message))
+            })
+        )
+    }
+
+    createVoiceChannel(input: StartMatchInput): Observable<Channel> {
         return this.httpService.post<Channel>(
             `${process.env.DISCORD_API_URL}/guilds/${process.env.SERVER_ID}/channels`,
             {
@@ -24,12 +77,29 @@ export class MatchService {
             }
         ).pipe(
             map((channelCreated) => {
-                console.log('channelCreated', channelCreated)
+                console.log('channelCreated', channelCreated.data)
                 return channelCreated.data
             }),
             catchError((err) => {
                 console.log('err', err)
                 return throwError(() => new InternalServerErrorException())
+            })
+        )
+    }
+
+    startVoiceConnections(): Observable<GatewayData> {
+        return this.httpService.get<GatewayData>(`${process.env.DISCORD_API_URL}/gateway/bot`, {
+            headers: {
+                Authorization: `Bot ${process.env.DISCORD_BOT_TOKEN}`
+            }
+        }).pipe(
+            map((res) => {
+                console.log('data', res.data)
+                return res.data;
+            }),
+            catchError((err) => {
+                console.log('err', err)
+                return throwError(() => new InternalServerErrorException(err.message))
             })
         )
     }
